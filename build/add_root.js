521d1b8e0e470d400ca0a64ca06c7c177d7266a0
Caolan McMahon <caolan@caolanmcmahon.com>
2011-12-19 12:55:23 -0800

    add postprocessor to always add root settings

--- build/add_root.js
+++ build/add_root.js
@@ -2,10 +2,6 @@ var utils = require('./utils');
 
 
 module.exports = function (root, path, settings, doc, callback) {
-    if (root) {
-        // the root settings are added by the post processor
-        return callback(null, doc);
-    }
     utils.prepareDoc(doc);
     try {
         utils.addSettings(root, doc, settings);
